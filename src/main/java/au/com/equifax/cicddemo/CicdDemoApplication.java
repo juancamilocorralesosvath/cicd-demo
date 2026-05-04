e9f7d548c0777cc38b8518a9a2c32755fb56fec2
juancamilocorralesosvath <juancamilocorra@gmail.com>
2026-05-03 19:33:06 -0500

    Test: Introduce security vulnerability for SonarQube

--- src/main/java/au/com/equifax/cicddemo/CicdDemoApplication.java
+++ src/main/java/au/com/equifax/cicddemo/CicdDemoApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CicdDemoApplication {
+
+    private static final String DB_PASSWORD = "super-secret-password-123";
+
     public static void main(String[] args) {
+        System.out.println("Connecting with password: " + DB_PASSWORD);
         SpringApplication.run(CicdDemoApplication.class, args);
     }
 }
